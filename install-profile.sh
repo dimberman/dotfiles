ad7ae0ac193f86250fec7c44f7bfbd9cab54789a
Daniel Imberman <danielryan2430@gmail.com>
2018-12-03 14:05:03 -0800

    fixed profile

--- install-profile.sh
+++ install-profile.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 
 cat ~/.bashrc > ~/.bashrc-old
-cat bashrc > .bashrc
+touch ~/.bashrc
+cat bashrc > ~/.bashrc
 cat ~/.bash_profile > ~/.bash_profile-old
-cat bash_profile >.bash_profile
+touch ~/.bash_profile
+cat bash_profile >~/.bash_profile
 cat ~/.inputrc > ~/.inputrc-old
-cat inputrc > .inputrc
-cat ~/.vimrc > ~/.vimrc-old
-cat vimrc > .vimrc
+touch ~/.inputrc
+cat inputrc > ~/.inputrc
+cat ~/.kube-ps1.sh > ~/.kube-ps1.sh-old
+touch ~/.kube-ps1.sh
+cat kube-ps1.sh > ~/.kube-ps1.sh
